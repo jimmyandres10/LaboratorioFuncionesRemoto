919ec90df38324f1a212368cc409c6a19a5834a7
Jimmy Andres Vera Montana <000410215@bga.upb>
2019-09-19 09:50:03 -0500

    a_power_b2

--- a_power_b.py
+++ a_power_b.py
@@ -3,11 +3,19 @@ expo= int(input("de el exponente del numero "))
 
 def a_power_b(base,expo):
     potencia=1
-    for i in range (1,expo+1):
-        potencia=potencia*expo
-        print ()
+    if base != 0:
 
-    return potencia
+        for i in range (1,expo+1):
+            potencia=potencia*expo
+            print ()
+    else:
+
+        print("su valor cero no es aceptable")
+
+        return potencia
 
 x=a_power_b(base,expo)
-print(f"su valor es {x}")
+if base!=0 :
+    print(f"su valor es {x}")
+
+
